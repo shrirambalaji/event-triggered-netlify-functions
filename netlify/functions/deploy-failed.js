38e5ae407b183efc6722f43ec7f4d9c466b5bf13
@shrirambalaji <shrirambalaji@Shrirams-MacBook-Pro.local>
2022-03-29 12:48:52 +0530

    log params

--- netlify/functions/deploy-failed.js
+++ netlify/functions/deploy-failed.js
@@ -1,30 +1,31 @@
 const fetch = require("node-fetch");
 
-exports.handler = async () => {
-  const giphyApiKey = process.env.GIPHY_API_KEY;
-  const slackApiSecret = process.env.SLACK_API_SECRET;
-  const channelID = process.env.SLACK_CHANNEL_ID;
-  const userID = process.env.SLACK_USER_ID;
+exports.handler = async ({ ...params }) => {
+  console.log("params", params);
+  // const giphyApiKey = process.env.GIPHY_API_KEY;
+  // const slackApiSecret = process.env.SLACK_API_SECRET;
+  // const channelID = process.env.SLACK_CHANNEL_ID;
+  // const userID = process.env.SLACK_USER_ID;
 
-  const response = await fetch(
-    `http://api.giphy.com/v1/gifs/random?tag=fail&api_key=${giphyApiKey}&limit=1`
-  );
-  const { data } = await response.json();
+  // const response = await fetch(
+  //   `http://api.giphy.com/v1/gifs/random?tag=fail&api_key=${giphyApiKey}&limit=1`
+  // );
+  // const { data } = await response.json();
 
-  if (data) {
-    await fetch(
-      `https://hooks.slack.com/services/${userID}/${channelID}/${slackApiSecret}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          text: data.url,
-        }),
-      }
-    );
-  }
+  // if (data) {
+  //   await fetch(
+  //     `https://hooks.slack.com/services/${userID}/${channelID}/${slackApiSecret}`,
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         text: data.url,
+  //       }),
+  //     }
+  //   );
+  // }
 
   return {
     statusCode: 200,
